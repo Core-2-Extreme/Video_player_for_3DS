c506b4f77b193120c1f82fcfdf3335b2a751fa22
Core-2-Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2022-03-03 17:22:40 +0900

    Follow pthread_attr_t value

--- source/system/util/fake_pthread.cpp
+++ source/system/util/fake_pthread.cpp
@@ -138,7 +138,11 @@ int	pthread_create(pthread_t *__pthread, const pthread_attr_t  *__attr, void *(*
     if(util_fake_pthread_enabled_cores == 0)
         return -1;
 
-    handle = threadCreate((ThreadFunc)__start_routine, __arg, DEF_STACKSIZE, DEF_THREAD_PRIORITY_LOW, util_fake_pthread_enabled_core_list[util_fake_pthread_core_offset], true);
+    if(__attr && __attr->is_initialized)
+        handle = threadCreate((ThreadFunc)__start_routine, __arg, __attr->stacksize, DEF_THREAD_PRIORITY_LOW, util_fake_pthread_enabled_core_list[util_fake_pthread_core_offset], true);
+    else
+        handle = threadCreate((ThreadFunc)__start_routine, __arg, DEF_STACKSIZE, DEF_THREAD_PRIORITY_LOW, util_fake_pthread_enabled_core_list[util_fake_pthread_core_offset], true);
+
     *__pthread = (pthread_t)handle;
 
     if(util_fake_pthread_core_offset + 1 < util_fake_pthread_enabled_cores)

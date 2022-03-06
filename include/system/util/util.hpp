f732c5dee379e3aa69dc65b648c96448285cca8a
Core-2-Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2022-03-06 12:55:07 +0900

    Moved function declaration

--- include/system/util/util.hpp
+++ include/system/util/util.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+extern "C" void* __real_malloc(size_t size);
+extern "C" void* __real_realloc(void* ptr, size_t size);
+extern "C" void __real_free(void* ptr);
+extern "C" void __real__free_r(struct _reent *r, void* ptr);
+extern "C" void* __real_memalign(size_t alignment, size_t size);
+
 /**
  * @brief Initialize util API.
  * @return On success DEF_SUCCESS, 
--- source/system/util/util.cpp
+++ source/system/util/util.cpp
@@ -10,12 +10,6 @@ Watch_string util_draw_watch_string[DEF_DRAW_MAX_WATCH_STRING_VARIABLES];
 
 extern "C" void memcpy_asm(u8*, u8*, int);
 
-extern "C" void* __real_malloc(size_t size);
-extern "C" void* __real_realloc(void* ptr, size_t size);
-extern "C" void __real_free(void* ptr);
-extern "C" void __real__free_r(struct _reent *r, void* ptr);
-extern "C" void* __real_memalign(size_t alignment, size_t size);
-
 extern "C" void* __wrap_malloc(size_t size)
 {
 	//Alloc memory on linear ram if requested size is greater than 32KB to prevent slow down (linear alloc is slow).

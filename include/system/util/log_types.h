3f751f5708cd11ee713eece897726d30f6676965
Core 2 Extreme <45873899+Core-2-Extreme@users.noreply.github.com>
2025-10-21 21:29:17 +0900

    Reverted log color

--- include/system/util/log_types.h
+++ include/system/util/log_types.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 #define DEF_LOG_BUFFER_LINES			(uint32_t)(512)
-#define DEF_LOG_COLOR					(uint32_t)(0xFF00BB60)
+#define DEF_LOG_COLOR					(uint32_t)(0xFFBBBB00)
 #define DEF_LOG_MAX_LENGTH				(uint32_t)(1024)			//Maximum log text length in bytes.
 #define DEF_LOG_INDEX_AUTO				(uint32_t)(UINT32_MAX)		//Use next log index.
 #define DEF_LOG_GET_SYMBOL(x)			#x							//Get symbol name.
